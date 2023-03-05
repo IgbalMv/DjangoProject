64923c300a19ac0eb0249e045060f9ca6df6d7c2
IgbalMv <mamedov_iqbal@mail.ru>
2023-03-05 21:19:34 +0400

    Contact problem

--- home/views.py
+++ home/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from django.http import (   HttpResponse,
+                            JsonResponse)
+
 
 def index(request):
     return render(request, 'index.html')
+
+
+def contact(request):
+    return render(template_name='contact.html',request=request)
